07fcab37f208978500719042296b645c4634c05a
tania <taniaosano15@gmail.com>
2022-10-06 16:19:34 +0300

    adding classnames to the page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Input from "./components/Input";
 
 function App() {
   return (
     <div className="App">
-     
-    </div>
+       <div className="weather">
+      <Input 
+      />
+
+      <div className="weather_display">
+        <h3 className="weather_location">Weather in Tokyo</h3>
+        <div>
+          <h1 className="weather_degrees">Temp: 32°C</h1>
+        </div>
+        <div className="weather_description">
+          <div>
+                <h3>cloudy</h3>
+            </div>
+                <h3>Humidity: %</h3>
+                <h3>Wind speed:m/s</h3>
+          </div>
+        </div>
+      </div>
+    </div>  
   );
 }
 
---
+++ src/components/Input.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./input.css";
+import { FiSearch } from 'react-icons/fi';
+
+const Input = () => {
+  return (
+    <form >
+        <input type={"text"} className="input_value" placeholder = "Please Enter Location" />
+        <FiSearch />
+        
+    </form>
+  )
+}
+
+export default Input
